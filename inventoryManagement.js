00baa51c8432c5a7f3e4cd61268f3bafe4a95bfb
Your Full Name <karumenick@gmail.com>
2026-09-24 13:37:26 +0300

    Updated module exports section

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,15 +1,7 @@
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
-
 const products = ["Laptop", "Phone", "Headphones", "Monitor"]
 
 function logFirstProduct(){
-  console.log(products[0]);
+  console.log(products);
 }
 function addProduct(newProduct){
   products.push(newProduct);
@@ -21,3 +13,10 @@ function updateProductName(position, newName){
 function removeLastProduct(){
   products.pop();
 }
+module.exports = {
+  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
+  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
+  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
+  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+  products
+};
